f5405c4f4779623cbb1c46f3e2aadc0df68a3d41
ananyatewari <ananyatewari0205@gmail.com>
2023-12-23 17:54:40 +0530

    project completed

--- script.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.getElementById("hamburger");
-const list = document.getElementsByClassName("lists");
-
-hamburger.addEventListener("click", () => {
-    for (let i = 0; i < list.length; i++){
-    list[i].classList.toggle('active');
-    }
-});
